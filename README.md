2310e9ae04bc5ba20917821f60d0e17894da666b
EJ-David <daviderhabor.de@gmail.com>
2023-05-22 01:37:20 +0100

    put in home page content

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Immerse-VR
+  
+
+  todo
+  - style the home page, aside aside.
+  - flex for tiny comp, grid for layout.
+  - 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import img from '../assets/image 81.png'
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-    <
-      <h1>Watch Movies In Virtual Reality</h1>
-      <p>Download and watch offline wherever you are</p>
-      <button>sign up</button>
+    <div >
+      <main>
+        <h1>Watch Movies In Virtual Reality</h1>
+        <p>Download and watch offline wherever you are</p>
+        <button>sign up</button>
+      </main>
+      <aside>
+        <img src={img}></img>
+      </aside>
     </div>
   );
 };
