8f119c72349ce9ae91c431da3f853c4999975519
EJ-David <daviderhabor.de@gmail.com>
2023-05-22 01:31:02 +0100

    warm up

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+    <
+      <h1>Watch Movies In Virtual Reality</h1>
+      <p>Download and watch offline wherever you are</p>
+      <button>sign up</button>
+    </div>
+  );
+};
+
+export default Home;
