46475c8b0f0e14dba95bedf5b2b3a8ccc979c513
“David-Ejiro” <teerex.trx@gmail.com>
2024-01-17 18:05:16 +0100

    styled movie watch page. with data recieved from API

--- src/Components/movieCard/movieCard.jsx
+++ src/Components/movieCard/movieCard.jsx
@@ -7,10 +7,13 @@ function MovieCard({ poster_path, title, overview, vote_average }) {
   let imgSrc = `https://image.tmdb.org/t/p/original${poster_path}`;
   return (
     <div className="mv-card">
-      <div>{title}</div>
-      <img src={imgSrc}></img>
-      <p className="hidden">{overview}</p>
-      <p className="hidden">Rating: {vote_average}</p>
+      <img src={imgSrc} alt="Movie Poster"></img>
+      <div id="descr">
+        <h3>{title}</h3>
+        <p className="hidden">{overview}</p>
+        <p className="hidden">Rating: {vote_average}</p>
+      </div>
+      <hr></hr>
     </div>
   );
 }
